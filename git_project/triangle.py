a7be91bbe0524431a597c5527f9147ec7947554e
Neil Patel <neilkpatel@gmail.com>
2019-04-03 11:10:53 -0400

    Added to comment

--- git_project/triangle.py
+++ git_project/triangle.py
@@ -2,7 +2,7 @@ import turtle
 
 painter = turtle.Turtle()
 
-for _ in range(3): # _ shows variable not needed great
+for _ in range(3): # _ shows variable not needed great cool
 	painter.forward(100)
 	painter.left(120)
 
